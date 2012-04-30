847acfbccb94f592a0bd6553b028557506cbf0cb
Tashi Trieu <tashi@Tashis-Hackintosh.local>
2012-04-29 19:15:42 -0700

    Initial Commit

---
+++ EDL Tool/TTDocument.h
@@ -0,0 +1,13 @@
+//
+//  TTDocument.h
+//  EDL Tool
+//
+//  Created by Tashi Trieu on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TTDocument : NSDocument
+
+@end
---
+++ EDL Tool/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EDL Tool
+//
+//  Created by Tashi Trieu on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
